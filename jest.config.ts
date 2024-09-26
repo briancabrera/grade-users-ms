020465a3fec47cc4b644e209fdf277adb26306ec
Brian Cabrera <briancabrer4@gmail.com>
2024-09-26 05:37:13 -0300

    [CONFIG] git ignore, jest, deps and ts config

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+};
+
+export default config;
