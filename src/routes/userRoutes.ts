6421e0cfcf70174e6470a8164fd4eec3bab0a270
Brian Cabrera <briancabrer4@gmail.com>
2024-09-26 06:30:15 -0300

    [ROUTING] changes at userRoutes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,36 +1,32 @@
+// src/routes/userRoutes.ts
+
 import express from 'express';
 import { Request, Response, NextFunction } from 'express';
 import {
+  createUser,
   getCurrentUser,
   updateUser,
   deleteUser
 } from '../controllers/userController';
+import { checkAuth } from '../middlewares/checkAuth';
 import { AuthenticatedRequest } from '../types/types';
 
 const userRouter = express.Router();
 
-userRouter.get('/me', async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
-    try {
-      getCurrentUser(req, res);
-    } catch (error) {
-      next(error);
-    }
+userRouter.post('/create', checkAuth, (req: Request, res: Response, next: NextFunction) => {
+  createUser(req as AuthenticatedRequest, res).catch(next);
 });
 
-userRouter.put('/update', async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
-    try {
-        await updateUser(req, res, next);
-    } catch (error) {
-        next(error);
-    }
+userRouter.get('/me', checkAuth, (req: Request, res: Response, next: NextFunction) => {
+  getCurrentUser(req as AuthenticatedRequest, res);
 });
 
-userRouter.delete('/delete', async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
-    try {
-      await deleteUser(req, res);
-    } catch (error) {
-      next(error);
-    }
+userRouter.put('/update', checkAuth, (req: Request, res: Response, next: NextFunction) => {
+  updateUser(req as AuthenticatedRequest, res, next).catch(next);
+});
+
+userRouter.delete('/delete', checkAuth, (req: Request, res: Response, next: NextFunction) => {
+  deleteUser(req as AuthenticatedRequest, res).catch(next);
 });
 
 export default userRouter;
